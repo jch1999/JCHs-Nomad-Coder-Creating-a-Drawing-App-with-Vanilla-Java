9962e418efcf1d3d37c97b8a9c184852b2698036
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-06-08 23:27:04 +0900

    1.3~1.5

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -5,35 +5,45 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-
-//fill 단색으로 모양을 채움
-//stroke 모양을 채우지 않음.
-// ctx.fillRect(50, 50, 100, 200);
-// ctx.strokeRect(200, 50, 100, 200);
-// ctx.rect(50, 50, 100, 100);
+//fillRect,strokeRect는 shortcut이다 그런데 rect도 short이다!
+//context는 0,0에서 시작하는 브러시이다. 원하는 위치로 이동시킬 필요가 있다.
+// ctx.moveTo(50,50);
+//이동시킨 곳에서 다른 곳으로 선을 긋는다
+// ctx.lineTo(150,50);
+// ctx.lineTo(150,150);
+// ctx.lineTo(50,150);
+// ctx.lineTo(50,50);
 // ctx.fill();
+
+//집 그리기
+// ctx.fillRect(200,200,50,200);
+// ctx.fillRect(400,200,50,200);
+ctx.lineWidth=2;//순서 중요! 먼저 넓이를 설정해 줘야 다음 stroke에 적용됨
+// ctx.strokeRect(300,300,50,100);
+// ctx.fillRect(300,300,50,100);
+// ctx.fillRect(200,200,200,20);
+// ctx.moveTo(200,200);
+// ctx.lineTo(325,100);
+// ctx.lineTo(450,200);
+// ctx.lineTo(200,200);
 // ctx.stroke();
-// ctx.rect(50, 50, 100, 100);
-// ctx.rect(150, 150, 100, 100);
-// ctx.rect(250, 250, 100, 100);
+// ctx.fillStyle="red";
 // ctx.fill();
-// ctx.rect(350, 350, 100, 100);
-// ctx.fillStyle = "red";
-//path: layer와 같은 개녕? path가 같을 경우
-// 같은 패스의 다른 것이 변경되면 나머지도 같이 바뀐다.
-// setTimeout(() => {
-//   ctx.fill();
-// }, 5000);
 
-//선을 긋는다.
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
+//사람 그리기
+ctx.fillRect(215,200,15,100);
+ctx.fillRect(350,200,15,100);
+ctx.fillRect(260,200,60,200);
+
+//원 (머리) 그리기 arc 사용
+ctx.arc(290,150,50,0,2*Math.PI);
 ctx.fill();
 
-//선을 끊고 새로 긋는다
+//눈 그리기
 ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
+ctx.fillStyle="white";
+// ctx.arc(270,150,5,0,2*Math.PI);
+// ctx.arc(310,150,5,0,2*Math.PI);
+ctx.arc(270,140,5,Math.PI,2*Math.PI);
+ctx.arc(310,140,5,Math.PI,2*Math.PI);
 ctx.fill();
